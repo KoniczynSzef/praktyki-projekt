2376de992cf9685b5536fb128d7b5cd36de6184e
KoniczynSzef <koniczynszef@gmail.com>
2025-03-03 12:09:27 +0100

    get suggested courses function

--- backend/Services/CourseService.cs
+++ backend/Services/CourseService.cs
@@ -13,9 +13,31 @@ public class CourseService : ICourseService
     return await db.Courses.ToListAsync();
   }
 
-  public Task<Course?> GetCourseById(int id) => throw new NotImplementedException();
+  public Task<Course> GetCourseById(int id) => throw new NotImplementedException();
 
-  public Task<IEnumerable<Course>> GetSuggestedCoursesByCourseId(int id) => throw new NotImplementedException();
+  public async Task<IEnumerable<Course>> GetSuggestedCoursesByCourseId(int id)
+  {
+    var course = await this.GetCourseById(id);
+    var allCourses = await this.GetAllCourses();
+
+    List<Course> suggestedCourses = new List<Course>();
+
+    foreach (var c in allCourses)
+    {
+      if (suggestedCourses.Count == 3)
+      {
+        return suggestedCourses;
+      }
+
+      if (c.Badge == course.Badge || c.Instructor == course.Instructor)
+      {
+        suggestedCourses.Add(c);
+      }
+    }
+
+    return suggestedCourses;
+  }
+
   public Task<IEnumerable<Course>> GetFeaturedCourses() => throw new NotImplementedException();
 
   public Task<Course> CreateCourse(Course course) => throw new NotImplementedException();
