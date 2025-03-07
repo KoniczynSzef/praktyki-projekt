835f067aa9cd6731401822295ab91150c803b4bf
KoniczynSzef <koniczynszef@gmail.com>
2025-03-07 12:17:24 +0100

    applied another migration, this time with inheriting the identity context

--- backend/Database/DatabaseContext.cs
+++ backend/Database/DatabaseContext.cs
@@ -1,16 +1,18 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
-public class DatabaseContext : DbContext
+public class DatabaseContext : IdentityDbContext<User, IdentityRole<string>, string>
 {
   public DbSet<Course> Courses { get; set; }
-  public DbSet<User> Users { get; set; }
   public DbSet<UserCourse> UserCourses { get; set; }
 
-  public DatabaseContext() { }
   public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options) { }
 
   protected override void OnModelCreating(ModelBuilder modelBuilder)
   {
+    base.OnModelCreating(modelBuilder);
+
     modelBuilder.Entity<UserCourse>()
         .HasKey(uc => new { uc.UserId, uc.CourseId });
 
